c747aeb2a01bf4907620fbe70f7f19122116aae8
Scot Wells <wells.scot@gmail.com>
2015-06-25 01:45:52 -0400

    finish content and add form

--- js/main.js
+++ js/main.js
@@ -11,4 +11,32 @@ $(function() {
       }
     }
   });
+
+  $('form').submit(function (event) {
+      event.preventDefault();
+
+      var data = {
+        name:    $(this).find('[name="name"]').val(),
+        email:   $(this).find('[name="email"]').val(),
+        subject: $(this).find('[name="subject"]').val(),
+        message: $(this).find('[name="message"]').val()
+      }
+
+      swal({
+        title: "Please wait!",
+        text: "Sending your message...",
+        type: "info",
+        allowEscapeKey: false,
+        showConfirmButton: false
+      });
+
+      $.ajax({
+        url: "http://getsimpleform.com/messages/ajax?form_api_token=99c927ae5658745cd0a05a6d7c0bec89",
+        dataType: 'jsonp',
+        data: data
+      }).success(function (data) {
+        swal("Thank you!", "Your message was sent! You should receive a reply shortly!", "success");
+        $('input, textarea').val('');
+      });
+  });
 });
